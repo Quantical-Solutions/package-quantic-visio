5ee2c64838c189b1b067e61573be74ef9026ace4
Fred-QS <in-touch-fg@quanticalsolutions.com>
2020-09-01 21:43:31 +0200

    update

--- src/Visio.php
+++ src/Visio.php
@@ -4,8 +4,31 @@ namespace Quantic\Visio;
 
 class Visio
 {
+    private static array $assets;
+    private static array $scripts;
+    private static string $favicon;
+
     public static function ignite()
     {
-        return 'test';
+        define('CHECKRENDER', true);
+        ob_start();
+        require_once(__DIR__ . '/public/index.php');
+        $content = ob_get_clean();
+        echo $content;
+    }
+
+    public static function addFavicon($url)
+    {
+        self::$favicon = $url;
+    }
+
+    public static function addAssets($assets)
+    {
+        self::$assets = implode('\\n', $assets);
+    }
+
+    public static function addScripts($scripts)
+    {
+        self::$scripts = implode('\\n', $scripts);
     }
 }
